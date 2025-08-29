dfb6d8ec369c29eb1ceda2407b72e6e742f89df2
Kamilas <mrbrogopro@gmail.com>
2025-08-29 11:19:14 +0200

    fixed navbar transparent color

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,18 +7,20 @@ import "./navbar.scss";
 const Navbar: React.FC = () => {
   const [Ypos, setYPos] = useState(true);
   const [isMobile, setIsMobile] = useState(false);
+  window.onresize = () => {
+    if (window.innerWidth > 1024) {
+      setIsMobile(false);
+    }
+  };
   window.onscroll = () => {
+    if (window.innerWidth <= 1024) return;
     if (window.scrollY === 0) {
       setYPos(true);
     } else {
       setYPos(false);
     }
   };
-  window.onresize = () => {
-    if (window.innerWidth > 1024) {
-      setIsMobile(false);
-    }
-  };
+
 
   if (isMobile) {
     (document.querySelector("html") as HTMLElement).style.overflowY = "hidden";
@@ -28,7 +30,7 @@ const Navbar: React.FC = () => {
   return (
     <>
       <nav
-        className={`${!Ypos && "scrolled"} navbar w-screen flex items-center justify-center fixed z-10`}
+        className={`${Ypos ? "" : "scrolled"} navbar w-screen flex items-center justify-center fixed z-10`}
       >
         <div className="inner-nav flex items-center justify-between">
           <HamburgerSvg className="hamburger" onClick={() => setIsMobile(!isMobile)} />
